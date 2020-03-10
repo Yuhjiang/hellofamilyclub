b4b6187f45d0a09fa7704c6376dccbadb3460964
jiangyuhao <jiang.yuhao0809@gmail.com>
2020-03-10 19:07:25 +0800

    修改生日字段和分页

--- blog/pagination.py
+++ blog/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class ListPagination(LimitOffsetPagination):
-    default_limit = 20
+    default_limit = 10
     limit_query_param = 'limited'
     offset_query_param = 'offset'
--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,9 @@ from .models import HelloUser
 
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(read_only=True)
+    phone = serializers.CharField(allow_blank=True)
 
     class Meta:
         model = HelloUser
-        fields = ('id', 'username', 'nickname', 'email', 'phone', 'avatar', 'birthday')
+        fields = ('id', 'username', 'nickname', 'email', 'phone', 'avatar', 'birthday',
+                  'is_admin')
